85f3ba32bcfdaeb2d9c7bf91d434fc290ddfe778
Amizx2 <anix.ig2005@gmail.com>
2023-10-05 20:20:51 +0300

    dophm2

--- HomeWork.py
+++ HomeWork.py
@@ -3,8 +3,25 @@ num = str(input('Enter a number: ' ))
 result_num = ''
 for x in num:
     result_num += str(int(x) ** 2)
-print('Result: ', result_num
+print('Result: ', result_num)
 
 #small варинат
 num = input('Enter a number: ')
 print(''.join(str(int(x) ** 2) for x in num))
+
+#1 Вычислить и вывести на экран сумму кубов натуральных чисел от 1 до n включительно. Верхний предел должен вводиться с клавиатуры и не должен превышать числа 100.
+n = int(input("Введите верхний предел (не более 100): "))
+if n > 100:
+    print("Верхний предел не должен превышать 100..")
+else:
+    summ = 0
+    for i in range(1, n + 1):
+        summ += i ** 3
+    print(f"Сумма кубов натуральных чисел от 1 до {n} равна {summ}.")
+
+
+#2 Выведите на экран таблицу умножения чисел от одного до девяти.
+for i in range(1, 10):
+    for j in range(1, 10):
+        print(i * j, end='\t')
+    print()
--- HomeWorkPlus.py
+++ HomeWorkPlus.py
@@ -1,60 +1,10 @@
-#1 Написать программу, которая будет делить введенные пользователем два вещественных числа и выводить результат на экран, сообщая об ошибке в случае деления на ноль.
-num1 = float(input("Введите первое число: "))
-num2 = float(input("Введите второе число: "))
+#Дополнительное задание Welcome. In this kata, you are asked to square every digit of a number and concatenate them.For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1. (81-1-1-81)Example #2: An input of 765 will/should return 493625 because 72 is 49, 62 is 36, and 52 is 25. (49-36-25)Note: The function accepts an integer and returns an integer.
+num = str(input('Enter a number: ' ))
+result_num = ''
+for x in num:
+    result_num += str(int(x) ** 2)
+print('Result: ', result_num)
 
-if num2 == 0:
-    print("Ошибка: Деление на ноль невозможно.")
-else:
-    result = num1 / num2
-    print("Результат:", result)
-
-#2 Рассчитать стоимость покупки с учетом скидки в 35%, которая предоставляется покупателю, если сумма покупки превышает 20 у.е. Сумму покупки ввести с клавиатуры, а результаты округлить до сотых (копейки, центы и т.д.). Вывести на экран итоговую стоимость и размер предоставленной скидки.
-total = float(input("Введите сумму покупки: "))
-if total > 20:
-    sale = total * 0.65
-    print("Сумма со скидкой: ", (round(sale, 2)))
-else:
-    print("Сумма недостаточна для скидки: ", total)
-
-#3 Напишите скрипт, который по введенному пользователем числу от 1 до 12, будет выводить на экран сообщение в виде названия месяца и времени года. Если пользователь введет недопустимое число, программа должна выдавать сообщение об ошибке.
-
-month_number = int(input("Введите число от 1 до 12: "))
-
-months = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"]
-
-seasons = ["Зима", "Весна", "Лето", "Осень"]
-
-if 1 <= month_number <= 12:
-        month = months[month_number - 1]
-        season = seasons[(month_number % 12) // 3]
-        print("Вы выбрали месяц",month , "и это",season)
-else:
-    print("Ошибка: Введите число от 1 до 12.")
-
-
-#ДОП ЗАДАНИЕ
-#Представьте вещественные числа 275.4, 0.0032 и 3.45 в коде Python в стандартном виде. Напомним, что в математике число в такой форме имеет вид a*10**n, где 1 ≤ a < 10, а n – целое число. Например, для числа 0.123 получим 1.23*10-1
-num1 = 275.4
-num2 = 0.0032
-num3 = 3.45
-def StandardForm(num):
-    stepen = 0
-    if num != 0:
-        while abs(num) >= 10:
-            num = num / 10
-            stepen += 1
-        while abs(num) < 1:
-            num = num * 10
-            stepen -= 1
-
-    formatted_num = f"{num:.2f} * 10^{stepen}"
-    return formatted_num
-format_num1 = StandardForm(num1)
-format_num2 = StandardForm(num2)
-format_num3 = StandardForm(num3)
-
-print(format_num1)
-print(format_num2)
-print(format_num3)
-
-
+#small варинат
+num = input('Enter a number: ')
+print(''.join(str(int(x) ** 2) for x in num))
