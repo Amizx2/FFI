edc64143df6a80b7a8433d54048fc8eb888e8cd4
Amizx2 <anix.ig2005@gmail.com>
2023-10-12 23:56:33 +0300

    HomeWork4

--- HomeWork.py
+++ HomeWork.py
@@ -1,29 +1,33 @@
-#Дополнительное задание Welcome. In this kata, you are asked to square every digit of a number and concatenate them.For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1. (81-1-1-81)Example #2: An input of 765 will/should return 493625 because 72 is 49, 62 is 36, and 52 is 25. (49-36-25)Note: The function accepts an integer and returns an integer.
-num = str(input('Enter a number: ' ))
-result_num = ''
-for x in num:
-    result_num += str(int(x) ** 2)
-print('Result: ', result_num)
+# 1 Дана строка. Подсчитать самую длинную последовательность подряд идущих букв «н». Преобразовать ее, заменив точками все восклицательные знаки.
+st = str(input('Введите строку:'))
+st = st.replace('!', '.')
+k = 1
+kmax = 0
+for i in range(len(st) - 1):
+    for g in range(i + 1, len(st)):
+        if st[i] == 'н' and st[g] == 'н':
+            k += 1
+            kmax = max(k, kmax)
+        else:
+            k = 1
+print(f'Ваше количество букв(н):{kmax}, измененная строка:{st}')
 
-#small варинат
-num = input('Enter a number: ')
-print(''.join(str(int(x) ** 2) for x in num))
 
-#1 Вычислить и вывести на экран сумму кубов натуральных чисел от 1 до n включительно. Верхний предел должен вводиться с клавиатуры и не должен превышать числа 100.
-n = int(input("Введите верхний предел (не более 100): "))
-if n > 100:
-    print("Верхний предел не должен превышать 100..")
+#2 Дана строка символов, среди которых есть одна открывающаяся и одна закрывающаяся скобки. Вывести на экран все символы, расположенные внутри этих скобок.
+st = str(input('Введите строку:'))
+opn = st.find('(')
+cls = st.find(')')
+print(opn)
+if opn != -1 and cls != -1:
+    insk = st[opn + 1:cls]
+    print(insk)
 else:
-    summ = 0
-    for i in range(1, n + 1):
-        summ += i ** 3
-    print(f"Сумма кубов натуральных чисел от 1 до {n} равна {summ}.")
+    print('Скобки не найдены')
 
 
-#2 Выведите на экран таблицу умножения чисел от одного до девяти.
-for i in range(1, 10):
-    for j in range(1, 10):
-        print(i * j, end='\t')
-    print()
-#Мой github https://github.com/Amizx2
-#В прошлом задании вы не засчитали мне дополнительное задание, оно также вложено как и это, оба задания есть в моем репозитории на гите, файл HomeWorkPlus
+#3 Дана строка. Вывести все слова, начинающиеся на букву "а" и слова оканчивающиеся на букву "я".
+st = str(input('Введите строку:'))
+words = st.split()
+for word in words:
+    if word.lower().startswith('а') and word.lower().endswith('я'):
+        print(word)
