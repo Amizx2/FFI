c4ff9dab048c36f1fc16773ee1ffa29f857f7748
Amizx2 <anix.ig2005@gmail.com>
2023-10-26 23:57:48 +0300

    Home work plus 4

--- HomeWorkPlus.py
+++ HomeWorkPlus.py
@@ -1,29 +1,12 @@
-#Task I need a wristband. Help me in identifying an actual wristband. A wristband can have 4 patterns: horizontal: each item in a row is identical. vertical: each item in each column is identical. diagonal left: each item is identical to the one on it's upper left or bottom right. diagonal right: each item is identical to the one on it's upper right or bottom left. Write a function that returns True if the section can be correctly classified into one of the 4 types, and False otherwise.
-def wristband(matrix):
-    Flag_horizontal = True
-    Flag_vertical = True
-    Flag_diagonal_left = True
-    Flag_diagonal_right = True
-    for row in matrix:
-        if len(set(row)) != 1:
-            Flag_horizontal = False
-            break
-    for j in range(len(matrix[0])):
-        if len(set(row[j] for row in matrix)) != 1:
-            Flag_vertical = False
-            break
-    for i in range(1, len(matrix)):
-        for j in range(1, len(matrix[0])):
-            if matrix[i][j] != matrix[i - 1][j - 1]:
-                Flag_diagonal_left = False
-                break
-    for i in range(1, len(matrix)):
-        for j in range(len(matrix[0]) - 1):
-            if matrix[i][j] != matrix[i - 1][j + 1]:
-                Flag_diagonal_right = False
-                break
-    return Flag_horizontal or Flag_vertical or Flag_diagonal_left or Flag_diagonal_right
-print(wristband([["A", "A"], ["B", "B"], ["C", "C"]]))
-print(wristband([["A", "B"], ["A", "B"], ["A", "B"]]))
-print(wristband([["A", "B", "C"], ["C", "A", "B"], ["B", "C", "A"], ["A", "B", "C"]]))
-print(wristband([["A", "B", "C"], ["B", "C", "A"], ["C", "A", "B"], ["A", "B", "A"]]))
+#There's no such thing as private properties on a coffeescript object! But, maybe there are? Implement a function createSecretHolder(secret) which accepts any value as secret and returns an object with ONLY two methods getSecret() which returns the secretsetSecret() which sets the secret obj = createSecretHolder(5) obj.getSecret() # returns 5 obj.setSecret(2) obj.getSecret() # returns 2
+class SecretHolder:
+    def __init__(self, secret):
+        self.__secret = secret
+    def getSecret(self):
+        return self.__secret
+    def setSecret(self, new_secret):
+        self.__secret = new_secret
+obj = SecretHolder(5)
+print(obj.getSecret())
+obj.setSecret(2)
+print(obj.getSecret())
