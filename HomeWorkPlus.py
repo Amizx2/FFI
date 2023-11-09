3ce9574c7f24078df6cb58373c7bd04b0a5daab7
Amizx2 <anix.ig2005@gmail.com>
2023-11-09 20:55:51 +0300

    Home Work Plus 7

--- HomeWorkPlus.py
+++ HomeWorkPlus.py
@@ -1,12 +1,11 @@
-#There's no such thing as private properties on a coffeescript object! But, maybe there are? Implement a function createSecretHolder(secret) which accepts any value as secret and returns an object with ONLY two methods getSecret() which returns the secretsetSecret() which sets the secret obj = createSecretHolder(5) obj.getSecret() # returns 5 obj.setSecret(2) obj.getSecret() # returns 2
-class SecretHolder:
-    def __init__(self, secret):
-        self.__secret = secret
-    def getSecret(self):
-        return self.__secret
-    def setSecret(self, new_secret):
-        self.__secret = new_secret
-obj = SecretHolder(5)
-print(obj.getSecret())
-obj.setSecret(2)
-print(obj.getSecret())
+#We have an array of unique elements. A special kind of permutation is the one that has all of its elements in a different position than the original. Let's see how many of these permutations may be generated from an array of four elements. We put the original array with square brackets and the wanted permutations with parentheses.  A total of 9 permutations with all their elements in different positions than arr The task for this kata would be to create a code to count all these permutations for an array of certain length. Features of the random tests: l = length of the array 10 ≤ l ≤ 5000 See the example tests. Enjoy it!
+from math import factorial
+
+def count(arr):
+    total_permutations = factorial(len(arr))
+    special_permutations = total_permutations // 2
+
+    return special_permutations
+arr = [1, 2, 3, 4]
+result = count(arr)
+print(result)
