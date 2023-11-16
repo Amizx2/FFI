9145691e6a3c228c1d3f46face4d93af90ad1afb
Amizx2 <anix.ig2005@gmail.com>
2023-11-17 01:06:26 +0300

    Home work Plus 9

--- HomeWorkPlus.py
+++ HomeWorkPlus.py
@@ -1,3 +1,4 @@
+#Here is a simple task. Take an array/tuple of unique positive integers, and two additional positive integers. Here's an example below: arr = (3,5,7,1,6,8,2,4) n = 3 # span length q = 13 # weight threshold Try to re-arrange arr so that the sum of any n consecutive values does not exceed q. solver(arr,n,q) ## one possible solution: (4,7,1,5,6,2,3,8) Did you succeed? Great! Now teach a computer to do it. Technical Details All test inputs will be valid All test cases will have 0 or more possible solutions If a test case has no solution, return an empty array/tuple. Otherwise, return a valid solutio    Test constraints: 2 <= n <= 6 4 <= arr length < 12 n < arr length Every value in arr will be less than q 11 fixed tests, 25 random tests
 def solver(arr, n, q):
     if len(arr) < n or max(arr) > q:
         return tuple()
